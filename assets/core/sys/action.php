818040175652ebce96648999c4e55c5529936faa
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-10 14:14:30 +0700

    adding validation if email exist

--- assets/core/sys/action.php
+++ assets/core/sys/action.php
@@ -5,14 +5,24 @@ if(@$_POST['key'] == 31337){
     $timeRightnow = date('Y/m/d H:i:s');
     switch ($_POST['action']) {
         case 0:
-            $queryRegister = "INSERT INTO members (id, nama, password, email, created_at) 
-            VALUES (AI_CATEGORIES.nextval, ?, ?, ?, TO_DATE('$timeRightnow', 'yyyy/mm/dd hh24:mi:ss'))";
-            $ekseQueryRegister = $pdo->prepare($queryRegister);
-            $isValid = $ekseQueryRegister->execute([
-                $_POST['newNamaLengkap'],
-                $_POST['newPassword'],
-                $_POST['newEmail']
-            ]);
+            $cekEmail = "SELECT COUNT(id) as total FROM members WHERE email = ?";
+            $ekseCekEmail = $pdo->prepare($cekEmail);
+            $ekseCekEmail->execute([$_POST['newEmail']]);
+            $hasil = $ekseCekEmail->fetchColumn();
+
+            if($hasil == 0){
+                $queryRegister = "INSERT INTO members (id, nama, password, email, created_at) 
+                VALUES (AI_CATEGORIES.nextval, ?, ?, ?, TO_DATE('$timeRightnow', 'yyyy/mm/dd hh24:mi:ss'))";
+                $ekseQueryRegister = $pdo->prepare($queryRegister);
+                $isValid = $ekseQueryRegister->execute([
+                    $_POST['newNamaLengkap'],
+                    $_POST['newPassword'],
+                    $_POST['newEmail']
+                ]);
+                if($isValid){ echo "ok"; }
+            }else{
+                echo "emailexist";
+            }
             break;
 
         case 1:
