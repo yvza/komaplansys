5da103f0ec517f3a0af988e9c36222bd2b953b91
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-10 14:13:45 +0700

    fix to date wrong format

--- assets/core/sys/action.php
+++ assets/core/sys/action.php
@@ -6,19 +6,13 @@ if(@$_POST['key'] == 31337){
     switch ($_POST['action']) {
         case 0:
             $queryRegister = "INSERT INTO members (id, nama, password, email, created_at) 
-                VALUES (?, ?, ?, ?, ?)";
+            VALUES (AI_CATEGORIES.nextval, ?, ?, ?, TO_DATE('$timeRightnow', 'yyyy/mm/dd hh24:mi:ss'))";
             $ekseQueryRegister = $pdo->prepare($queryRegister);
-            try {
-                $ekseQueryRegister->execute([
-                    AI_CATEGORIES.nextval,
-                    $_POST['newNamaLengkap'],
-                    $_POST['newPassword'],
-                    $_POST['newEmail'],
-                    TO_DATE($timeRightnow, 'yyyy/mm/dd hh24:mi:ss')
-                ]);
-            } catch (\Throwable $th) {
-                throw $th;
-            }
+            $isValid = $ekseQueryRegister->execute([
+                $_POST['newNamaLengkap'],
+                $_POST['newPassword'],
+                $_POST['newEmail']
+            ]);
             break;
 
         case 1:
