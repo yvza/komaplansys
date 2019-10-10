6fb13d9ac61e548ae0839d1382f693002f9bc17a
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-10 10:42:12 +0700

    finishing markup

--- whatreudoing.php
+++ whatreudoing.php
@@ -8,36 +8,36 @@
     <link rel="stylesheet" href="./assets/whatreudoing/style.css">
 </head>
 <body>
-    <div class="form-collection">
+    <div class="form-collection" id="app">
         <div class="card elevation-3 limit-width log-in-card below turned">
             <div class="card-body">
                 <div class="input-group email">
                     <input type="text" placeholder="Email"/>
                 </div>
-                <div class="input-group password">
-                    <input type="password" placeholder="Password"/>
-                </div>
-                <a href="#" class="box-btn">Forgot Password?</a>
+                    <div class="input-group password">
+                        <input type="password" placeholder="Password"/>
+                    </div>
+                    <a href="!#" class="box-btn">Lupa Password?</a>
                 </div>
                 <div class="card-footer">
-                <button type="submit" class="login-btn">Log in</button>
+                    <button type="submit" class="login-btn">MASUK</button>
                 </div>
             </div>
 
             <div class="card elevation-2 limit-width sign-up-card above">
                 <div class="card-body">
-                <div class="input-group fullname">
-                    <input type="text" placeholder="Full Name"/>
-                </div>
-                <div class="input-group email">
-                    <input type="email" placeholder="Email"/>
-                </div>
-                <div class="input-group password">
-                    <input type="password" placeholder="Password"/>
-                </div>
+                    <div class="input-group fullname">
+                        <input type="text" placeholder="Nama Lengkap"/>
+                    </div>
+                    <div class="input-group email">
+                        <input type="email" placeholder="Email"/>
+                    </div>
+                    <div class="input-group password">
+                        <input type="password" placeholder="Password"/>
+                    </div>
                 </div>
                 <div class="card-footer">
-                <button type="submit" class="signup-btn">Sign Up</button>
+                    <button type="submit" class="signup-btn">DAFTAR</button>
                 </div>
             </div>
         </div>
