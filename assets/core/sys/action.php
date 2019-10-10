4222faf06b0800c820b66ec9d5ef78fa5165544a
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-10 11:52:59 +0700

    code block for action daftar

---
+++ assets/core/sys/action.php
@@ -0,0 +1,29 @@
+<?php
+require_once('./login.php');
+
+if($_POST['key'] === 31337){
+    $timeRightnow = date('Y/m/d H:i:s');
+    switch ($_POST['action']) {
+        case 0:
+            $queryRegister = "INSERT INTO members (id, nama, password, email, created_at) 
+                VALUES (?, ?, ?, ?, ?)";
+            $ekseQueryRegister = $pdo->prepare($queryRegister);
+            try {
+                $ekseQueryRegister->execute([
+                    AI_CATEGORIES.nextval,
+                    $_POST['newNamaLengkap'],
+                    $_POST['newPassword'],
+                    $_POST['newEmail'],
+                    TO_DATE($timeRightnow, 'yyyy/mm/dd hh24:mi:ss')
+                ]);
+            } catch (\Throwable $th) {
+                throw $th;
+            }
+            break;
+
+        case 1:
+            # code...
+            break;
+    }
+}
+?>
