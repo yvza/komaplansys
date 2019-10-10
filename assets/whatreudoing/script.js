bbd2e5ca5d95ae4f48f0f4197d8bda2fef9c3873
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-10 10:43:17 +0700

    moving animate config into created lifecycle vuejs

--- assets/whatreudoing/script.js
+++ assets/whatreudoing/script.js
@@ -1,21 +1,39 @@
-$(document).on('click', '.below button', function(){
-    var belowCard = $('.below'),
-    aboveCard = $('.above'),
-    parent = $('.form-collection')
-    parent.addClass('animation-state-1')
-    setTimeout(function(){
-        belowCard.removeClass('below')
-        aboveCard.removeClass('above')
-        belowCard.addClass('above')
-        aboveCard.addClass('below')
-        setTimeout(function(){
-        parent.addClass('animation-state-finish')
-        parent.removeClass('animation-state-1')
-        setTimeout(function(){
-            aboveCard.addClass('turned')
-            belowCard.removeClass('turned')
-            parent.removeClass('animation-state-finish')
-        }, 300)
-        }, 10)
-    }, 300)
-})
+const x = {
+    data() {
+        return {
+            
+        }
+    },
+    created() {
+        $(document).on('click', '.below button', function(){
+            var belowCard = $('.below'),
+            aboveCard = $('.above'),
+            parent = $('.form-collection')
+            parent.addClass('animation-state-1')
+            setTimeout(function(){
+                belowCard.removeClass('below')
+                aboveCard.removeClass('above')
+                belowCard.addClass('above')
+                aboveCard.addClass('below')
+                setTimeout(function(){
+                parent.addClass('animation-state-finish')
+                parent.removeClass('animation-state-1')
+                setTimeout(function(){
+                    aboveCard.addClass('turned')
+                    belowCard.removeClass('turned')
+                    parent.removeClass('animation-state-finish')
+                }, 300)
+                }, 10)
+            }, 300)
+        })
+    },
+    mounted() {
+        
+    },
+    methods: {
+        
+    }
+}
+
+const app = new Vue(x)
+app.$mount('#app')
