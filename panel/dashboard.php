11d54c0836ee51839fd707d1285717104bbc3250
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-11 22:06:57 +0700

    changing title

--- panel/dashboard.php
+++ panel/dashboard.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>User Control!</title>
+    <title>Dashboard - KOMAPLANSYS</title>
     <link rel="shortcut icon" href="../assets/core/img/logouwu.png" type="image/x-icon">
     <link rel="stylesheet" href="../node_modules/buefy/dist/buefy.min.css">
     <link rel="stylesheet" href="../node_modules/@fullcalendar/core/main.min.css">
