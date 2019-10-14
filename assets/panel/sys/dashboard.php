6563ee7faf9507650e2a5091fe691f49732ca6c1
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-15 00:23:05 +0700

    update query if item moving

--- assets/panel/sys/dashboard.php
+++ assets/panel/sys/dashboard.php
@@ -5,9 +5,34 @@ if(!isset($_SESSION['members_id'])){
     header('Location: ../../whatreudoing.php');
 }
 
-if(@$_GET['get'] === "userinfo"){
+if(@$_GET['get'] === "allinfo"){
+    // 0: info user || 1. schedule
+    $arrHelper = [];
     $queryGetUserInfo = "SELECT * FROM members WHERE id = {$_SESSION['members_id']}";
     $ekse = $pdo->query($queryGetUserInfo)->fetch();
-    echo json_encode($ekse);
+    array_push($arrHelper, $ekse);
+
+    $query = "SELECT title, start_date, end_date FROM plansys WHERE user_id = {$_SESSION['members_id']}";
+    $ekse = $pdo->query($query)->fetchAll();
+    array_push($arrHelper, []);
+    foreach ($ekse as $key => $value) {
+        $costumArray = [
+            'title' => $value['TITLE'],
+            'start' => $value['START_DATE'],
+            'end' => $value['END_DATE']
+        ];
+        array_push($arrHelper[1], $costumArray);
+    }
+    echo json_encode($arrHelper);
 }
-?>
+
+if(@$_GET['moving'] === "schedule"){
+    $query = "UPDATE plansys SET start_date = ?, end_date = ? WHERE title = ?";
+    $ekse = $pdo->prepare($query);
+    $res = $ekse->execute([
+        $_POST['startDate'],
+        $_POST['endDate'],
+        $_POST['title']
+    ]);
+    if($res){ echo "ok"; }
+}
