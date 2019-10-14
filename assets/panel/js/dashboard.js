d265f2e2eb96b6c18a98877522333f5450036f3c
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-15 00:22:28 +0700

    drap and drop functionality

--- assets/panel/js/dashboard.js
+++ assets/panel/js/dashboard.js
@@ -2,46 +2,69 @@ const x = {
     data() {
         return {
             //self info
-            nama: null
+            nama: null,
+            //schedule
+            list: [],
+            //store drag and drop
+            startDate: null,
+            endDate: null
         }
     },
     created() {
-        //get user info
-        $.ajax({
-            type: 'GET',
-            url: '../assets/panel/sys/dashboard.php?get=userinfo',
-            success: function(res){
-                let json = JSON.parse(res)
-                app.nama = json.NAMA
-            }
-        })
+
     },
     mounted() {
-        var calendarEl = document.getElementById('calendar')
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-            plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
-            timeZone: 'UTC',
-            header: {
-                left: 'prev,next today',
-                center: 'title',
-                right: 'dayGridMonth,timeGridWeek,timeGridDay' //dayGridMonth,timeGridWeek,timeGridDay
-            },
-            editable: true,
-            eventLimit: true,
-            dateClick: function(info) {
-                alert('Clicked on: ' + info.dateStr);
-                alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
-                alert('Current view: ' + info.view.type);
-            },
-            events: [
-                { // this object will be "parsed" into an Event Object
-                    title: 'Tessss', // a property!
-                    start: '2019-10-05', // a property!
-                    end: '2019-10-16' // a property! ** see important note below about 'end' **
-                }
-            ]
+        $.ajax({
+            type: 'GET',
+            url: '../assets/panel/sys/dashboard.php?get=allinfo',
+            success: function(res){
+                let json = JSON.parse(res)
+                app.nama = json[0].NAMA
+                app.list = json[1]
+                var calendarEl = document.getElementById('calendar')
+                var calendar = new FullCalendar.Calendar(calendarEl, {
+                    plugins: [ 'interaction', 'dayGrid', 'timeGrid' ],
+                    timeZone: 'UTC',
+                    header: {
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay' //dayGridMonth,timeGridWeek,timeGridDay
+                    },
+                    editable: true,
+                    selectable: true,
+                    eventLimit: true,
+                    eventDrop: function(info) {
+                        function standart(dateobject){ //date manipulation
+                            function pad(n){return n<10 ? '0'+n : n}
+                            return pad(dateobject.getFullYear())+"-"+
+                            pad(dateobject.getMonth()+1)+"-"+
+                            pad(dateobject.getDate())
+                        }
+                        app.startDate = standart(info.event.start),
+                        app.endDate = standart(info.event.end)
+                        let title = info.event.title
+                        app.$buefy.dialog.confirm({
+                            message: 'Yakin anda merubah tanggal schedule?',
+                            onConfirm: () => $.ajax({
+                                type: "POST",
+                                data: "title="+title+
+                                "&startDate="+app.startDate+
+                                "&endDate="+app.endDate,
+                                url: "../assets/panel/sys/dashboard.php?moving=schedule",
+                                success: function(res){
+                                    if(res === 'ok'){
+                                        app.$buefy.toast.open({message: "Sukses lurr!", type: 'is-success'})
+                                    }
+                                }
+                            }),
+                            onCancel: () => info.revert()
+                        })
+                    },
+                    events: app.list
+                })
+                calendar.render()
+            }
         })
-        calendar.render()
 
         $(".navbar-burger").click(function() {
             $("#navbarBasicExample").slideToggle()
