0b3bd387eb102749298a0ddc2cfd12ae328192b6
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-15 00:21:38 +0700

    fix hamburger menu

--- assets/panel/js/planning.js
+++ assets/panel/js/planning.js
@@ -79,7 +79,11 @@ const x = {
         }
     },
     mounted() {
-        
+        $(".navbar-burger").click(function() {
+            $("#navbarBasicExample").slideToggle()
+            $(".navbar-burger").toggleClass("is-active")
+            $(".navbar-menu").toggleClass("is-active")
+        })
     },
     methods: {
         keluar(event){
