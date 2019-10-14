1ea06e5a6c827e0514eaff5cea15b0b2b4b3bc15
Akhmad Yuza <akhmadyuza@gmail.com>
2019-10-15 03:15:55 +0700

    all functionality

--- assets/panel/sys/result.php
+++ assets/panel/sys/result.php
@@ -4,4 +4,17 @@ require_once('../../core/sys/connection.php');
 if(!isset($_SESSION['members_id'])){
     header('Location: ../../whatreudoing.php');
 }
+
+$arrHelper = [];
+
+$query = "SELECT count(id) as total FROM plansys WHERE review < 3";
+array_push($arrHelper, $pdo->query($query)->fetch());
+
+$query = "SELECT count(id) as total FROM plansys WHERE review >= 3 AND review < 5";
+array_push($arrHelper, $pdo->query($query)->fetch());
+
+$query = "SELECT count(id) as total FROM plansys WHERE review >= 5";
+array_push($arrHelper, $pdo->query($query)->fetch());
+
+echo json_encode($arrHelper);
 ?>
