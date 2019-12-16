e4acc41779b5320a06a7ceb72bc9258bd268f616
Akhmad Yuza <akhmadyuza@gmail.com>
2019-12-16 20:21:26 +0700

    adding color picker, ckeditor 5 configuration

--- assets/panel/js/planning.js
+++ assets/panel/js/planning.js
@@ -73,12 +73,14 @@ const x = {
             sortIconSize: 'is-small',
             currentPage: 1,
             perPage: 20,
-            //config insert new event
+            // config insert new event
             isCardModalActive: false,
             dates: [],
-            //dropper
+            // dropper
             categories: null,
-            status: null
+            status: null,
+            // create data switcher
+            isSwitched: false
         }
     },
     created() {
@@ -98,6 +100,43 @@ const x = {
             $("#navbarBasicExample").slideToggle()
             $(".navbar-burger").toggleClass("is-active")
             $(".navbar-menu").toggleClass("is-active")
+        })
+        // color picker configuration
+        const pickr = Pickr.create({
+            el: '.color-picker',
+            theme: 'nano', // or 'monolith', or 'nano'
+            swatches: [
+                'rgba(244, 67, 54, 1)',
+                'rgba(233, 30, 99, 0.95)',
+                'rgba(156, 39, 176, 0.9)',
+                'rgba(103, 58, 183, 0.85)',
+                'rgba(63, 81, 181, 0.8)',
+                'rgba(33, 150, 243, 0.75)',
+                'rgba(3, 169, 244, 0.7)',
+                'rgba(0, 188, 212, 0.7)',
+                'rgba(0, 150, 136, 0.75)',
+                'rgba(76, 175, 80, 0.8)',
+                'rgba(139, 195, 74, 0.85)',
+                'rgba(205, 220, 57, 0.9)',
+                'rgba(255, 235, 59, 0.95)',
+                'rgba(255, 193, 7, 1)'
+            ],
+            components: {
+                // Main components
+                preview: true,
+                opacity: true,
+                hue: true,
+                // Input / output Options
+                interaction: {
+                    save: true
+                }
+            }
+        })
+        // ckeditor 5 config
+        ClassicEditor
+            .create( document.querySelector( '#editor' ) )
+            .catch( error => {
+                console.error( error );
         })
     },
     methods: {
