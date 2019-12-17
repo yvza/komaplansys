72ec20ab078f7334a07677466488767bbf896d66
Akhmad Yuza <akhmadyuza@gmail.com>
2019-12-17 14:47:28 +0700

    implementing update query

--- assets/panel/sys/planning.php
+++ assets/panel/sys/planning.php
@@ -96,4 +96,10 @@ if(@$_GET['get'] === "view"){
     $note = $pdo->query("SELECT note FROM plansys WHERE id = {$_POST['id']}")->fetch()['NOTE'];
     echo json_encode($note);
 }
+
+if(@$_GET['update'] === "note"){
+    $ekse = $pdo->prepare("UPDATE plansys SET note = ? WHERE id = ?");
+    $res = $ekse->execute([$_POST['newNote'], $_POST['id']]);
+    if($res){ echo "ok"; }
+}
 ?>
