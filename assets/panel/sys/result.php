110db072c4a905be81680fc5dbb420d1e5113736
Akhmad Yuza <akhmadyuza@gmail.com>
2019-12-17 12:50:55 +0700

    fix incorrect field

--- assets/panel/sys/result.php
+++ assets/panel/sys/result.php
@@ -7,13 +7,13 @@ if(!isset($_SESSION['members_id'])){
 
 $arrHelper = [];
 
-$query = "SELECT count(id) AS total FROM plansys WHERE category = 3 AND user_id = {$_SESSION['members_id']}";
+$query = "SELECT count(id) AS total FROM plansys WHERE review = 3 AND user_id = {$_SESSION['members_id']}";
 array_push($arrHelper, $pdo->query($query)->fetch());
 
-$query = "SELECT count(id) AS total FROM plansys WHERE category = 2 AND user_id = {$_SESSION['members_id']}";
+$query = "SELECT count(id) AS total FROM plansys WHERE review = 2 AND user_id = {$_SESSION['members_id']}";
 array_push($arrHelper, $pdo->query($query)->fetch());
 
-$query = "SELECT count(id) AS total FROM plansys WHERE category = 1 AND user_id = {$_SESSION['members_id']}";
+$query = "SELECT count(id) AS total FROM plansys WHERE review = 1 AND user_id = {$_SESSION['members_id']}";
 array_push($arrHelper, $pdo->query($query)->fetch());
 
 echo json_encode($arrHelper);
