0d1d8d6e6f179dc2a6277c2a4ec6b6ca658d7466
Akhmad Yuza <akhmadyuza@gmail.com>
2019-12-17 10:54:32 +0700

    deleting stars method

--- assets/panel/js/planning.js
+++ assets/panel/js/planning.js
@@ -256,13 +256,6 @@ const x = {
                 }
             })
         },
-        starsChange(id, value){
-            $.ajax({
-                type: "POST",
-                url: "../assets/panel/sys/planning.php?change=stars",
-                data: "id="+id+"&value="+value
-            })
-        },
         categoryChanged(id){
             let category = $('#category').val()
             $.ajax({
